0ca86ccfd67019a185f7653f0839e2609d7730fe
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-16 19:29:08 +0100

    Encrypt user text with hardcoded number

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,7 +1,30 @@
 def caesar_cipher(str)
+  encrypt_key = 4
+  encrypted_str = ""
 
+  str.each_char do |char|
+    unless char == " "
+      ascii_code = char.ord - encrypt_key
+      
+      if char.ord >= 97 && char.ord <= 122 
+        if ascii_code < 97
+          ascii_code = 123 - (97 - ascii_code)
+        end
+      elsif char.ord >= 65 && char.ord <= 90
+        if ascii_code < 65
+          ascii_code = 91 - (65 - ascii_code)
+        end
+      end
+
+      encrypted_str += ascii_code.chr
+    else
+      encrypted_str += char
+    end
+  end
+
+  encrypted_str
 end
 
 str = gets.chomp
 
-p str
+puts caesar_cipher(str)
